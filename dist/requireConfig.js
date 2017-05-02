13fd14eb9965c633eadade54114cb7089601983b
Xu <kai.xu01@sap.com>
2017-05-02 15:07:15 +0800

    init

---
+++ dist/requireConfig.js
@@ -0,0 +1,24 @@
+ requirejs.config({
+        //By default load any module IDs from js/lib
+        baseUrl: './dist',
+        paths:{
+                leaflet:"../lib/leaflet/dist/leaflet-src",
+                underscore:"../lib/underscore/underscore",
+                jquery:"../lib/jquery/dist/jquery",
+                bootstrap:"../lib/bootstrap/dist/js/bootstrap",
+                d3:"../lib/d3/d3",
+                text : "../lib/text/text"
+        },
+        shim:{
+                leaflet:{
+                        exports:"L"
+                },
+                "underscore": {
+                        exports: "_"
+                },
+                bootstrap:{
+                        deps:["jquery"]
+                }
+        }
+});
+
---
+++ src/BaseLayer.ts
@@ -0,0 +1,3 @@
+import d3 =require("d3")
+import _ =require("underscore")
+import Evented =require("Evented")
