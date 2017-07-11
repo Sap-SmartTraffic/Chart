38329a56c5c29c57aa4c73849d8faa42e54b3ab6
Pandaice <1542752218@qq.com>
2017-07-11 14:31:12 +0800

    update loader

--- src_new/Loader/ILoader.ts
+++ src_new/Loader/ILoader.ts
@@ -3,7 +3,7 @@ export interface IProgressLoader{
     setWidth(s:string):this
     setHeight(s:string):this
     remove():this
-    show():this    
+    show():this   
     toElement():HTMLElement|SVGAElement
     setProgress(n:number):this
 }
